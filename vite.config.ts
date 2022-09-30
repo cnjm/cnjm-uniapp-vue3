df84af26ec048058b85950a6eac07aaaa38a0cb7
szmfcjm_JfT <chenjiaming@shuzimofang.net>
2022-09-30 14:10:16 +0800

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import uni from "@dcloudio/vite-plugin-uni";
+import { resolve } from "path";
+function pathResolve(dir: string) {
+  return resolve(process.cwd(), ".", dir);
+}
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [uni()],
+  resolve: {
+    alias: [
+      {
+        find: /\/@\//,
+        replacement: pathResolve("src") + "/",
+      },
+    ],
+  },
+});
